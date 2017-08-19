16fcc06b341877a0564717e56d0d9c07472173fa
Bill Goggin <bgoggin@mac.com>
2017-08-19 18:26:16 -0400

    Added controls

--- Control Fun/ViewController.swift
+++ Control Fun/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var numberField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,11 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func textFieldDoneEditing(sender: UITextField)
+    {
+        sender.resignFirstResponder()
     }
 
 
